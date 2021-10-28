15c98661f4965b64f68ea834ed461d5081233deb
Nicholas Balderas <geist732@gmail.com>
2021-10-27 19:42:15 -0500

    Disabled debug in grid

--- Assets/Scripts/GridXZ.cs
+++ Assets/Scripts/GridXZ.cs
@@ -35,7 +35,7 @@ public class GridXZ<TGridObject>
             }
         }
 
-        bool showDebug = true;
+        bool showDebug = false;
         if (showDebug)
         {
             TextMesh[][] debugTextArray = new TextMesh[xAxisValues.Length][];
