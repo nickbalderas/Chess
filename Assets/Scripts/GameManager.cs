b1c18c0d3d5cc7ea4ed12808374011b64372f5e4
Nicholas Balderas <geist732@gmail.com>
2021-10-31 06:07:18 -0500

    Added player script. Started Pawn promotion but decided to create separate branch.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,21 @@
+using System.Collections.Generic;
+using structs;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public ChessPiece SelectedChessPiece { get; private set; }
+    public Player LightPlayer;
+    public Player DarkPlayer;
+    
+    private void Awake()
+    {
+        LightPlayer = new Player(new List<ChessPiece>(), new List<ChessPiece>(), new List<XZCoordinate>(), true);
+        DarkPlayer = new Player(new List<ChessPiece>(), new List<ChessPiece>(), new List<XZCoordinate>(), false);
+        LightPlayer.OpposingPlayer = DarkPlayer;
+        DarkPlayer.OpposingPlayer = LightPlayer;
+    }
 
     public void SetSelectedChessPiece(ChessPiece chessPiece)
     {
